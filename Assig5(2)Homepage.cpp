59cee9b5d95675b38674e6290ce85f7d8cb99e11
ALI ZIA KHAN <59319815+ALI-ZIA-KHAN@users.noreply.github.com>
2020-12-30 12:14:27 +0500

    Assignment6(part2 Homepage) by Ali Zia Khan

---
+++ Assig5(2)Homepage.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+            cout<<"*********Assignment 6 by ALI ZIA KHAN***********"<<endl;
+			cout<<"============= Welcome to Homepage ===============\n";
+			
+			cout<<"Press e for exit"<<endl;
+			cout<<"press anyother key to move on"<<endl;
+			cout<<"Select?"<<endl;
+			char toexit;
+			cin>>toexit;
+				
+				if(toexit=='e'){
+					exit(0);
+				}
+				else{
+				system("start Assig5(3)ATMNBP.exe");
+				
+			}
+			
+			
+		}
