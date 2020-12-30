e8c0a8f83e047e71ba22b1acda63c730d90dda82
ALI ZIA KHAN <59319815+ALI-ZIA-KHAN@users.noreply.github.com>
2020-12-30 12:04:27 +0500

    Assignment 6 By Ali Zia Khan

---
+++ Assignment6.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+	cout<<"==========MARKSHEET========="<<endl;
+	cout<<"Enter your English Subject Marks"<<endl;
+	float eng;
+	cin>>eng;
+	cout<<"Enter your Urdu subject marks"<<endl;
+	float urd;
+	cin>>urd;
+	cout<<"Enter your Mathematics subject marks"<<endl;
+	float mat;
+	cin>>mat;
+	cout<<"Enter your Science Subject Marks"<<endl;
+	float sci;
+	cin>>sci;
+	cout<<"Enter your computer subject marks"<<endl;
+	float comp;
+	cin>>comp;
+	
+	
+	float obt,per;
+	obt=sci+comp+eng+urd+mat;
+	int total=500;
+	per=(obt/total)*100;
+	
+	
+	
+	cout<<"\n\n\n"<<endl;
+	cout<<"============Result========="<<endl;
+    
+    cout<<"total marks:"<<total<<endl;
+    cout<<"Obtained marks:"<<obt<<endl;
+    cout<<"Percentage:"<<per<<"%"<<endl;
+	
+	return 0;
+	
+	
+	
+	
+	
+	
+}
