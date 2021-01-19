f6bbf3e4dd3e280913b5d7f6ebb9f52e07d31b57
ALI ZIA KHAN <59319815+ALI-ZIA-KHAN@users.noreply.github.com>
2021-01-19 06:18:58 +0500

    Assignment 7 by Ali Zia Khan

---
+++ Assignment7.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+
+int main(){
+	
+	int num;
+	cout<<"Enter the number of which table you want"<<endl;
+	cin>>num;
+	
+	for(int i=1;i<=10;i++){
+		
+		
+		cout<<num<<"  "<<"  *  "<<i<<"   "<<"   =  "<<num*i<<endl;
+		
+	}
+		
+		return 0;
+		
+	}
+
