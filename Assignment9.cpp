1bfc5cdae3548975579492ef43714e1777bc96e0
ALI ZIA KHAN <59319815+ALI-ZIA-KHAN@users.noreply.github.com>
2021-01-19 06:20:04 +0500

    Assignment 9 by Ali Zia Khan

---
+++ Assignment9.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+
+int main(){
+	
+	int rows;
+	cout<<"Enter the number of rows   ";
+	cin>>rows;
+	
+	for(int i=1;i<=rows;i++){
+		
+		for(int j=1;j<=i;j++){
+			
+			cout<<j;
+			
+		}
+		
+		cout<<"\n";
+	}
+	
+}
