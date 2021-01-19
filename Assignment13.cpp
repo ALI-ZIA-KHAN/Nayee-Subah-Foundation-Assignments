fc86dc24bfb24953d53c90b1b77c93f21c10c204
ALI ZIA KHAN <59319815+ALI-ZIA-KHAN@users.noreply.github.com>
2021-01-19 06:22:39 +0500

    Assignment 13 by Ali Zia Khan

---
+++ Assignment13.cpp
@@ -0,0 +1,21 @@
+/*Assignment No.13
+Print Five fruits name by user using 1D Array.*/
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+
+
+  string fruits[5]={"Apple","orange","banana","watermelon","pineapple"};
+  for(int i=0;i<5;i++){
+  	
+  	cout<<"The elements of fruits array are\t"<<fruits[i]<<endl;
+  	
+  	
+  }
+
+
+  return 0;
+
+}
