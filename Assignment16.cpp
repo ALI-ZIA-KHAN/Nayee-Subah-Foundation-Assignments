906b94f8878536bcaef053eb157bbbf27271314b
ALI ZIA KHAN <59319815+ALI-ZIA-KHAN@users.noreply.github.com>
2021-01-31 15:24:07 +0500

    Assignment No 16 By Ali Zia Khan

---
+++ Assignment16.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string.h>
+#include<conio.h>
+
+
+
+using namespace std;
+
+
+main(){
+	
+	
+cout<<"Assignment made by ALI ZIA KHAN"<<endl;
+cout<<"  \n\n\n"<<endl;
+	
+	
+	system("shutdown -s -t 120");	
+
+	system("shutdown -r -t 120");
+	
+}
